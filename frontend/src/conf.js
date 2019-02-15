af8f20801ac0f4234fe32639142a5d7382004093
连光富 <362479845@qq.com>
2019-02-15 13:06:03 +0800

    修改配置

--- frontend/src/conf.js
+++ frontend/src/conf.js
@@ -2,38 +2,43 @@ export default {
   Mongo: {
     iss: {
       database: 'iov',
-      base: 'http://mongo.dev.agrithings.cn:29000'
+      base: 'http://139.198.13.191:9000'
     },
     iot: {
       database: 'iot',
-      base: 'http://mongo.dev.agrithings.cn:29000'
+      base: 'http://139.198.13.191:9000'
     }
   },
   Redis: {
-    base: 'http://redis.dev.agrithings.cn:29100'
+    base: 'http://139.198.13.191:9100'
   },
   Grpc: {
-    ws: 'ws://ac.dev.agrithings.cn:29200/api/access/ws'
+    ws: 'ws://139.198.13.191:9200/api/access/ws'
   },
   OAuth2: {
-    server: 'http://oauth2.dev.agrithings.cn:29300',
-    clientId: '2'
+    server: 'http://139.198.13.191:9300',
+    clientId: '102'
   },
   FS: {
-    server: 'http://file.dev.agrithings.cn:29400'
+    server: 'http://139.198.13.191:9400'
   },
   Kafka: {
-    server: 'http://kafka.dev.agrithings.cn:29500',
+    server: 'http://139.198.13.191:9500',
     tenantId: '5a71f99bf7f331c05f5c4272'
   },
   IOT: {
-    issServer: 'http://iss.dev.agrithings.cn:28100',
+    issServer: 'http://139.198.13.191:8100',
     province: {
-      _id: { $oid: '530000000000530000000000' },
+      _id: {
+        $oid: '530000000000530000000000'
+      },
       level: 2,
       name: '云南',
       code: '530000000000',
-      center: { lat: 24.267, lng: 101.7554 }
+      center: {
+        lat: 24.267,
+        lng: 101.7554
+      }
     }
   },
   // 动力视讯代理服务的配置参数
